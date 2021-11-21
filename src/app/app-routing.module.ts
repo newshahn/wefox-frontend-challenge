225fb769ab9a3ed9671081e4919197d2fa354a8b
Newsha Hosseiny Nik <nioosha.h@gmail.com>
2021-11-21 20:19:10 +0330

    added some routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlacesComponent } from './components/places/places.component';
+import { PlaceDetailComponent } from './components/place-detail/place-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: PlacesComponent },
+  { path: 'detail/:id', component: PlaceDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
