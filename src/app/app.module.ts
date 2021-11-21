9c53daec70eef8689887718394cb49ce9478ec0b
Newsha Hosseiny Nik <nioosha.h@gmail.com>
2021-11-21 20:19:37 +0330

    added place list and a form for adding a new place

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PlacesComponent } from './components/places/places.component';
+import { PlaceDetailComponent } from './components/place-detail/place-detail.component';
+
+// Material Modules
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, PlacesComponent, PlaceDetailComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    //
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/places/places.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Place } from 'src/app/core/models/wefox-place.model';
+import { WefoxRESTService } from 'src/app/core/services/REST/wefox-rest.service';
+
+@Component({
+  selector: 'places',
+  templateUrl: './places.component.html',
+  styleUrls: ['./places.component.scss'],
+})
+export class PlacesComponent implements OnInit {
+  places: Place[] = [];
+  private newPlaceForm: FormGroup;
+
+  constructor(
+    private restService: WefoxRESTService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.getPlaces();
+
+    this.newPlaceForm = this.formBuilder.group({
+      title: ['', [Validators.required, Validators.maxLength(20)]],
+      content: ['', [Validators.required, Validators.maxLength(50)]],
+      lat: [''],
+      long: [''],
+      image_url: [''],
+    });
+  }
+
+  get newPlaceFormGroup(): FormGroup {
+    return this.newPlaceForm;
+  }
+
+  getPlaces(): void {
+    this.restService.getPlaces().subscribe((places) => (this.places = places));
+  }
+
+  add(title: string): void {
+    title = title.trim();
+    if (!title) {
+      return;
+    }
+    this.restService.addPlace({ title } as Place).subscribe((place) => {
+      console.log(`{place.title} was added to places`);
+    });
+  }
+
+  add2(): void {
+    let { title, content, ...dataToSend } = this.newPlaceForm.value;
+
+    if (!title?.trim() || !content?.trim()) {
+      return;
+    }
+    this.restService
+      .addPlace(this.newPlaceForm.value as Place)
+      .subscribe((place) => {
+        console.log(`{place.title} was added to places`);
+      });
+  }
+
+  delete(place: Place): void {
+    this.places = this.places.filter((p) => p !== place);
+    this.restService.deletePlace(place.id).subscribe();
+  }
+}
